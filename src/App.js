54f35fba9d870d7f9c95281afa657e1a4776fa1d
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2021-05-16 18:51:03 -0700

    added search box and styles component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import {CardList} from './components/card-list/card-list';
 import {Card} from './components/card/card';
+import {SearchBox} from './components/search-box/search-box'
 import './App.css';
 
 class App extends React.Component{
   constructor(){
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount(){
@@ -16,10 +19,20 @@ class App extends React.Component{
     fetch(url).then(response => response.json()).then(users => this.setState({monsters: users}))
   } 
 
+  handleChange(e) {
+    this.setState({searchField: e.target.value})
+  }
+
   render() {
+    const {monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}/>
+        <SearchBox 
+          placeholder='search monsters'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}/>
       </div>
     );
   }
